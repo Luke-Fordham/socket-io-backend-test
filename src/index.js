c56c36658279cedd251a4c2f8541c74c1cc8a3de
luke <luke@blueskylabs.com.au>
2021-07-23 11:43:34 +0800

    successfully get all user conversations from database

--- src/index.js
+++ src/index.js
@@ -1,102 +1,112 @@
 const http = require("http");
 const app = require("express")();
+const {PrismaClient} = require('@prisma/client')
+const prisma = new PrismaClient()
 
 const server = http.createServer(app);
 const io = require("socket.io")(server, {
-  cors: {
-    origin: 'http://localhost:3000'
-  }
+    cors: {
+        origin: 'http://localhost:3000'
+    }
 
 });
 
 // Enable CORS for all HTTP methods
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
-  res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-  next();
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    next();
 });
 
 
-const PORT = 8080;
+const PORT = 3001;
+
+app.get('/get-all-users', async (req, res) => {
+    let success = false;
+    let error = null;
+    let users = {};
+    const result = await prisma.sb_user.findMany();
+    if (result) {
+        success = true;
+        users = result;
+    } else {
+        error = 'No users found'
+    }
+    res.send({
+        success,
+        error,
+        users
+    })
+})
 
 io.use((socket, next) => {
-  const username = socket.handshake.auth.username;
-  if (!username) {
-    return next(new Error("invalid username"));
-  }
-  socket.username = username;
-  next();
+    const username = socket.handshake.auth.username;
+    const userId = socket.handshake.auth.userId;
+    if (!username || !userId) {
+        return next(new Error("invalid username/id"));
+    }
+    socket.username = username;
+    socket.userId = userId;
+    next();
 });
 
 const userList = [];
 
-io.on("connection", (socket) => {
-  const users = [];
+io.on("connection", async (socket) => {
 
-  for (let [id, socket] of io.of("/").sockets) {
-    users.push({
-      userID: id,
-      username: socket.username,
-      messages: [],
-      connected: true
+    const conversations = await prisma.$queryRaw(
+        `SELECT c.* FROM sb_conversation_member cm
+                             LEFT JOIN sb_conversation c ON cm.conversation_id = c.id
+                                WHERE cm.object_ref = 'sb_user'
+                                AND cm.object_id = ${socket.userId}
+                                `
+    )
+
+    console.log(conversations);
+
+
+    socket.on("private message", ({content, to}) => {
+        console.log({
+            content,
+            from: socket.userId,
+        });
+        socket.to(to).emit("private message", {
+            content,
+            from: socket.id,
+        });
     });
-  }
-  socket.emit("users", users);
-  // notify existing users
-  socket.broadcast.emit("user connected", {
-    userID: socket.id,
-    username: socket.username,
-    connected: true,
-    messages: [],
-  });
 
-  userList.push({
-    userID: socket.id,
-    username: socket.username,
-    connected: true,
-    messages: [],
-  });
-
-  socket.on("private message", ({ content, to }) => {
-    console.log({
-      content,
-      from: socket.id,
-    });
-    socket.to(to).emit("private message", {
-      content,
-      from: socket.id,
-    });
-  });
-
-  app.get('/get-single-user/:id', (req, res) => {
-    if (req.params.id) {
-      let success = false;
-      let error = null;
-      let user = {};
-      const result = userList.find(user => user.userID === req.params.id);
-      if (result) {
-        success = true;
-        user = result;
-      } else {
-        error = 'Could not find user with requested ID'
-      }
-      res.send({
-        success,
-        error,
-        user
-      })
-    }
-  })
+    app.get('/get-single-user/:id', async (req, res) => {
+        let success = false;
+        let error = null;
+        let user = {};
+        if (req.params.id) {
+            const result = await prisma.sb_user.findUnique({where: {id: req.params.id}});
+            if (result) {
+                success = true;
+                user = result;
+            } else {
+                error = 'Could not find user with requested ID'
+            }
+        } else {
+            error = 'user id required'
+        }
+        res.send({
+            success,
+            error,
+            user
+        })
+    })
 });
 
 
 // 3. Start the server
 server.listen(PORT, () =>
-  console.log(`
+    console.log(`
 👋
 Server started
 Socket host is listening for connections.
